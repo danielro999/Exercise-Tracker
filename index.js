e193e20cc2ecdf303eff1e170d2f02016b75d371
Daniel Gerez <persensus@yahoo.com.ar>
2022-09-22 21:15:58 +0000

    test aprobado, el filtro final muy enredado, se prodria hacer con metodos del mongoose

--- index.js
+++ index.js
@@ -36,10 +36,6 @@ const userSchema = new Schema({
 
 const User = mongoose.model("User", userSchema);
 
-
-// :You can POST to /api/users with form data username to create a new user.
-// :The returned response from POST /api/users with form data username will be an object with username and _id properties.
-              //      {"username":"pedro","_id":"63292c10c8e97a09397759c6"}
 count = "0";
 app.post('/api/users', function(req, res) {
   const username = req.body.username;
@@ -54,31 +50,13 @@ app.post('/api/users', function(req, res) {
   })  
 });
 
-// :You can make a GET request to /api/users to get a list of all users.
-// :The GET request to /api/users returns an array.
-// :Each element in the array returned from GET /api/users is an object literal containing a user's username and _id.
-          //   [     {"_id":"632915b4c8e97a09397759a8","username":"lucas","__v":0},
-                  // {"_id":"632916a8c8e97a09397759ae","username":"yuy","__v":0},
-                  // {"_id":"63291e00c8e97a09397759b1","username":"danil","__v":0},
-                  // {"_id":"63292373c8e97a09397759b8","username":"DANIEL","__v":0},
-                  // {"_id":"63292384c8e97a09397759bd","username":"DANIEL","__v":0},
-                  // {"_id":"63292b3ec8e97a09397759c0","username":"DANIEL","__v":0},
-                  // {"_id":"63292c10c8e97a09397759c6","username":"pedro","__v":0},
-                  // {"_id":"63292c4ec8e97a09397759c8","username":"pedro","__v":0} ]
-
 app.get('/api/users', function (req, res) 
  {
     User.find({}).sort({name:1}).exec((err, data) =>  {
       if (err) return console.log(err);
-     res.json(data);
+      res.json(data);
     }) 
  });
-
-
-// :You can POST to /api/users/:_id/exercises with form data description, duration, and optionally date. If no date is supplied, the current date will be used.        
-
-// :The response returned from POST /api/users/:_id/exercises will be the user object with the exercise fields added.
-      // {"_id":"63292384c8e97a09397759bd","username":"DANIEL","date":"Thu Dec 02 1999","duration":11,"description":"zaraza"}
 
 app.post('/api/users/:_id/exercises', function (req, res) {
   
@@ -86,55 +64,85 @@ app.post('/api/users/:_id/exercises', function (req, res) {
   const {description} = req.body;
   const duration = parseInt(req.body.duration);
   var date = req.body.date ? req.body.date : new Date();
-  // console.log ("date: " + req.body.date);
-  // console.log ("duration: " + req.body.duration);
-  
   const update={
     description,
     duration,
     date:new Date(date).toDateString()
     };
   
-  User.findByIdAndUpdate(_id, {$push:{log:update}}, {new: true}, (err, updatedDoc) => {
-   
-    
+  User.findByIdAndUpdate(_id, {$push:{log:update}}, {new: true}, (err, updatedDoc) => {   
     if(updatedDoc) {
       const updatedUser ={
       _id: updatedDoc._id,
       username: updatedDoc.username,
       ...update
       };
-  
       const arrayDate =  updatedDoc.log.date;
       const count = Object.values(arrayDate).length;
       //console.log("count= " + count );
       updatedDoc.count =count;
       updatedDoc.save();
-    
       res.json( updatedUser )
     }    
   }) 
 })
-
-
+//exercise-tracker.danielro999.repl.co/api/users/632c7c3b3a356700e2ef538d/logs?from=1990-01-01&to=2022-01-01&limit=5
 app.get('/api/users/:_id/logs',(req, res)=>
 {  
-   const _id = req.params._id;
+  const _id = req.params._id;
   const { from, to, limit } = req.query;
-
+  logss = new Array;
   const dateFrom = new Date(from);
   const dateTo = new Date(to);
-  // console.log(dateFrom,         
-  // dateTo,parseInt(limit), _id);
+//console.log(`Id = ${_id}   Desde = ${from} hasta = ${to} limitado por = ${limit}`)
+if (from || to || limit )
+  User.findById(_id,(err, data) =>
+  {
+    if (err)return console.log(err);
+    const arrayLogs =  Object.values(data.log)
+    logss = new Array();
+    cont = 0;
+    contIf= 0;
+    for ( log of arrayLogs[0]) {
+      const description = log;
+      const duration = arrayLogs[1][cont];
+      const date = new Date(arrayLogs[2][cont]); 
+      cont++; 
+   
+      if ((dateFrom <= date) & (date <= dateTo)) {
+          logss.push({
+            description: log,
+            duration: duration,
+            date: date.toDateString()
+            })
+          contIf++;
+          if (contIf === parseInt(limit)) { break; }
+        console.log(date)
+        }
+      if (!from) {
+        logss.push({
+            description: log,
+            duration: duration,
+            date: date.toDateString()
+            })
+          contIf++;
+          if (contIf === parseInt(limit)) { break; }
+      }      
+    }
 
- User.findById({_id}, (err,data)=>{
-      if (err)return console.log(err);
-        console.log(data);
-        res.json(data);
-        
-       })
-
-    
-
+    const user = {
+      _id : data._id,
+      username : data.username,
+     // from : dateFrom.toDateString(),
+      // to : dateTo.toDateString(),
+      count : data.count,
+      log : logss
+    }
+    res.send(user)
+  }); 
+else
+  User.findById(_id, (err, data) => {
+    if (err)return console.log(err);
+    res.json(data);       
+  })
 });
-
